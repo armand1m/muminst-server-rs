bb568ea667cc10a9ac971c7ca6167bbb5a9aaa67
Armando Magalhaes <amagalhaes@netflix.com>
2021-11-01 00:09:16 +0100

    style: fix indentation

--- migrations/2021-10-31-142001_create_sounds/up.sql
+++ migrations/2021-10-31-142001_create_sounds/up.sql
@@ -1,8 +1,8 @@
 -- Your SQL goes here
 CREATE TABLE sounds (
-	id TEXT NOT NULL PRIMARY KEY,
-	name TEXT NOT NULL,
-	extension TEXT NOT NULL,
-	file_name TEXT NOT NULL,
-	file_hash TEXT NOT NULL
+    id TEXT NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+    extension TEXT NOT NULL,
+    file_name TEXT NOT NULL,
+    file_hash TEXT NOT NULL
 );
--- migrations/2021-10-31-142923_create_tags/up.sql
+++ migrations/2021-10-31-142923_create_tags/up.sql
@@ -1,6 +1,6 @@
 -- Your SQL goes here
 CREATE TABLE tags (
-	id TEXT NOT NULL UNIQUE,
+    id TEXT NOT NULL UNIQUE,
     sound_id TEXT NOT NULL,
     slug TEXT NOT NULL,
     PRIMARY KEY (id, sound_id, slug),
